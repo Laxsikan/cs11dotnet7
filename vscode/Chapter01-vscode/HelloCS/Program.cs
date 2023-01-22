3dd5496138affc12c7454923fc207397845ece77
Laxsikan Manoranjan <manoranl@kean.edu>
2023-01-22 18:30:51 -0500

    First commit

--- vscode/Chapter01-vscode/HelloCS/Program.cs
+++ vscode/Chapter01-vscode/HelloCS/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
+Console.WriteLine("Hello, Laxsikan Manoranjan!");
+Console.WriteLine("Var is 1!");
+Console.WriteLine("Version:{0}", Environment.Version.ToString());
 throw new Exception();
-int z;
+int x;
+ 
